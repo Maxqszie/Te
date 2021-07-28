396e9c9650b3da40558befc56b7a74ce6713662a
小生不才 <88026070+Maxqszie@users.noreply.github.com>
2021-07-28 19:53:34 +0800

    Add files via upload

---
+++ 7.27.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int arr[]={10,9,8,7,5,6,4,3,2,1};
+	int temp=0;
+	int i=0;
+	f0r(i=0;i<10;i++)
+	{
+		if(a[i]>a[i+1])
+		{
+			temp=a[i+1];
+			a[i+1]=a[i];
+			a[i]=temp;
+		}
+		else
+		contine;
+	}
+	return 0;
+ } 
